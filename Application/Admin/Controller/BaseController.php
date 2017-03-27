319aca7752c3ba99e9e9725aa55b32fa7e9aafac
hanyingquan <hanyingquan@jtw.com>
2017-03-27 23:36:52 +0800

    添加Model

---
+++ Application/Admin/Controller/BaseController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 后台基础Controller类
+ * User: DarkVisitor
+ * Date: 2017/3/27
+ * Time: 23:29
+ */
+
+namespace Admin\Controller;
+
+use Think\Controller;
+class BaseController extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
---
+++ Application/Admin/Model/AdminModel,class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: DarkVisitor
+ * Date: 2017/3/27
+ * Time: 22:05
+ */
+namespace Admin\Model;
+
+use Think\Model;
+class AdminModel extends BaseModel
+{
+
+}
